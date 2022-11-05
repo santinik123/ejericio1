5098f4d8e6e56f6454e9eee8f624c329a7fb4655
santinik123 <116964370+santinik123@users.noreply.github.com>
2022-11-05 07:59:33 -0600

    Add files via upload

---
+++ Curso python/Hola_Mundo.py
@@ -0,0 +1,9 @@
+# EJEMPLO 1. SALUDO
+#  COMENTARIOS: sirven para documentar o detallar cada parte del codigo
+#  A los comentarios no los toma en ccuenta o los omiente Python
+
+#  Imprimiendo o mostrando mensajes o resultados
+print("Hola Mundo")
+print(' nico ');
+
+print(10+5)
---
+++ Curso python/operaciones.py
@@ -0,0 +1,40 @@
+#EJEMPLO 2. Operaciones Matematicas
+
+# ENTRADA DE DATOS
+# Declarar o crear las variable
+
+
+import math
+
+
+numero1 = 20
+numero2 = 10
+# Declarar una constate: Elemento que no cambia su valor
+PI = 3.1416
+
+# Procesos (Claculos y/o Operaciones Matematicas y Logicas)
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+potencia1 = numero1 ** numero2
+potencia2 = pow(numero1, numero2)
+cuadrado = numero1 ** numero2
+cubo = pow(numero2, 3)
+raiz_cuadrada1 = math.sqrt(6)
+raiz_cuadrada2 = pow(9, 1/2)
+raiz_cubica = pow(27, 1/3)
+
+modulo_residuo = numero1 % numero2
+
+# Salida de datos
+print("la suma es =", suma)
+print("la resta es =", resta)
+print("la multiplicacion es =", multiplicacion)
+print("la division es =", division)
+print("la potencia1 es =", potencia1)
+print("la potencia2 es =", potencia2)
+print("el cuadrado es =", cuadrado)
+print("el cubo es =", cubo)
+print("la raiz cuadrada1 es =", raiz_cuadrada1)
+print("la suma es =", suma)
